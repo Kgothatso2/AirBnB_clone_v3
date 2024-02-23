04836cac9c3a70f023f5130f9df1bbea53d90ee5
LeinadMori <iromdaniel@gmail.com>
2024-02-23 22:48:07 +0000

    Status of the API being checked

---
+++ v1/views/index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+from flask import jsonify, Blueprint
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """Return a JSON response with status OK."""
+    return jsonify({"status": "OK"})
