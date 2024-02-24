c676b80ad86ff0d4aeaf6345f70c1ea180e48258
LeinadMori <iromdaniel@gmail.com>
2024-02-24 00:45:36 +0000

    trying correcting issues and making the code better

--- v1/views/__init__.py
+++ v1/views/__init__.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__)
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+
 app_views.route('/api/v1/example', methods=['GET'])
-def example_route():
-return "Hello"
+@app_views.route('/status')
+def status_get():
+    return jsonify({'status': 'OK'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
